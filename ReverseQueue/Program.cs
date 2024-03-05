bb03263eb178fd800076ef39c71ec01ffa33f0fc
suleseid <122879276+suleseid@users.noreply.github.com>
2024-03-04 16:16:33 -0800

    Add project files.

---
+++ ReverseQueue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> originalQueue = new Queue<int>(new[] { 1, 2, 3, 4, 5 });
+
+            Queue queue = new Queue();
+            queue.ReverseQueue(originalQueue);
+
+            Console.WriteLine("New Order:");
+            foreach (int item in originalQueue)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
---
+++ ReverseQueue/Queue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseQueue
+{
+    public class Queue
+    {
+        public void ReverseQueue(Queue<int> queue)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            // Pop elements from the queue and push onto the stack
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+
+            // Pop elements from the stack and enqueue back into the queue
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+        }
+
+    }
+}
